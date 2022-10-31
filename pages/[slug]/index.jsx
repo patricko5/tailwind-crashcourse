18389f3b0d3f2a6a7f94e9da1b036e5eb2decc7d
Patrick Orihuela <patrick.orihuela@gmail.com>
2022-10-30 18:42:10 -0600

    Font family set up

---
+++ pages/[slug]/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogDetails = () => {
+  return (
+    <div>The Blog Details</div>
+  )
+}
+
+export default BlogDetails
---
+++ pages/add-blog/index.jsx
@@ -0,0 +1,8 @@
+
+const addBlog = () => {
+  return (
+    <div>add blog</div>
+  )
+}
+
+export default addBlog
